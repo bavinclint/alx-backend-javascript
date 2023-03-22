66d689f8e53b0648bf50e4c85faf99cac4525f4c
bavinclint <bavinclint@gmail.com>
2023-03-23 00:45:39 +0300

    Const or let?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,10 +1,10 @@
-export function getLast() {
-  return ' is okay';
-}
-
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
+}
+
+export function getLast() {
+  return ' is okay';
 }
 
 export function taskNext() {
